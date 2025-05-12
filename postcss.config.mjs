6143c2d40f396f8c50618f3c9c38ebd9c8711cd2
58mustafasahin <58.mustafasahin@gmail.com>
2025-05-12 14:22:10 +0300

    hero section

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -7,42 +7,43 @@ const config = {
 		"./sanity/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {
-			screens: {
-				xs: "475px",
-			},
-			colors: {
-				primary: {
-					"100": "#FFE8F0",
-					DEFAULT: "#EE2B69",
-				},
-				secondary: "#FBE843",
-				black: {
-					"100": "#333333",
-					"200": "#141413",
-					"300": "#7D8087",
-					DEFAULT: "#000000",
-				},
-				white: {
-					"100": "#F7F7F7",
-					DEFAULT: "#FFFFFF",
-				},
-			},
-			fontFamily: {
-				"work-sans": ["var(--font-work-sans)"],
-			},
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)",
-			},
-			boxShadow: {
-				100: "2px 2px 0px 0px rgb(0, 0, 0)",
-				200: "2px 2px 0px 2px rgb(0, 0, 0)",
-				300: "2px 2px 0px 2px rgb(238, 43, 105)",
-			},
-		},
-	},
+    extend: {
+      screens: {
+        xs: "var(--breakpoint-xs)", // 475px
+        '3xl': "var(--breakpoint-3xl)", // 1920px
+      },
+      colors: {
+        primari: {
+          100: "var(--color-primari-100)",
+          DEFAULT: "var(--color-primari)",
+        },
+        secondari: "var(--color-secondari)",
+        black: {
+          100: "var(--color-black-100)",
+          200: "var(--color-black-200)",
+          300: "var(--color-black-300)",
+          DEFAULT: "var(--color-black)",
+        },
+        white: {
+          100: "var(--color-white-100)",
+          DEFAULT: "var(--color-white)",
+        },
+      },
+      fontFamily: {
+        "work-sans": ["var(--font-work-sans)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        100: "2px 2px 0px 0px rgb(0, 0, 0)",
+        200: "2px 2px 0px 2px rgb(0, 0, 0)",
+        300: "2px 2px 0px 2px var(--color-primary)",
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
