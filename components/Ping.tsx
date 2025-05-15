61fb6a469545309274f909492479258db63f0fac
58mustafasahin <58.mustafasahin@gmail.com>
2025-05-15 15:36:42 +0300

    Startup Details

---
+++ components/Ping.tsx
@@ -0,0 +1,15 @@
+
+const Ping = () => {
+    return (
+        <div className="relative">
+            <div className="absolute -left-4 top-1">
+                <span className="flex size-[11px]">
+                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primari opacity-75"></span>
+                    <span className="relative inline-flex size-[11px] rounded-full bg-primari"></span>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Ping
